10b00c0718dbc26ac009513e4b1a69e4d5041d87
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-02-05 15:11:17 +0100

    First component extraction

---
+++ src/Tablero.jsx
@@ -0,0 +1,20 @@
+function Tablero() {
+  return (
+    <div className="game">
+      <h1>Turno de Pepe (X)</h1>
+      <div className="board">
+        <div className="cell">X</div>
+        <div className="cell">O</div>
+        <div className="cell"></div>
+        <div className="cell">X</div>
+        <div className="cell">X</div>
+        <div className="cell">O</div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Tablero
