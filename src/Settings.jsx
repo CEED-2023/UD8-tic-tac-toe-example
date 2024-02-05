a20bb2bce56fddc75ff7260f752525859dcd13cc
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-02-05 15:45:02 +0100

    Typo

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -8,7 +8,7 @@ function Settings() {
           <input id="jugador1" type="text" value="Pepe" />
         </div>
         <div>
-          <label htmlFor="caracter2">Caracter 2</label>
+          <label htmlFor="caracter2">Caracter 1</label>
           <input id="caracter1" type="text" maxLength="1" size="1" value="X"/>
         </div>
       </div>
@@ -18,7 +18,7 @@ function Settings() {
           <input id="jugador2" type="text" value="Juan"/>
         </div>
         <div>
-          <label htmlFor="caracter1">Caracter 1: </label>
+          <label htmlFor="caracter1">Caracter 2: </label>
           <input id="caracter1" type="text" maxLength="1" size="1" value="O"/>
         </div>
       </div>
