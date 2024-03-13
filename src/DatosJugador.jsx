7572ac47c7ae76d426f00c11441cc1b85fee5171
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-03-13 10:47:53 +0100

    Undoes useRef example

--- src/DatosJugador.jsx
+++ src/DatosJugador.jsx
@@ -1,25 +1,27 @@
-import { useRef } from 'react'
-
 function DatosJugador(props) {
 
   const { num } = props
   const { nombre, simbolo } = props
   const { onCambioNombre, onCambioSimbolo } = props
 
-  const refNombre = useRef()
-
   function cambioNombre(event) {
     onCambioNombre?.(num, event.target.value)
   }
 
   function cambioSimbolo(event) {
-    const nuevoSimbolo = event.target.value
-    onCambioSimbolo?.(num, nuevoSimbolo)
-    if(nuevoSimbolo.length === 1) refNombre.current.focus()
+    onCambioSimbolo?.(num, event.target.value)
   }
 
   return (
     <div className="row">
+      <div>
+        <label htmlFor={`jugador${num}`}>Jugador {num}: </label>
+        <input
+          type="text"
+          value={nombre}
+          onChange={cambioNombre}
+        />
+      </div>
       <div>
         <label htmlFor={`caracter${num}`}>Caracter {num}</label>
         <input
@@ -28,15 +30,6 @@ function DatosJugador(props) {
           size="1"
           value={simbolo}
           onChange={cambioSimbolo}
-        />
-      </div>
-      <div>
-        <label htmlFor={`jugador${num}`}>Jugador {num}: </label>
-        <input
-          type="text"
-          value={nombre}
-          onChange={cambioNombre}
-          ref={refNombre}
         />
       </div>
     </div>
