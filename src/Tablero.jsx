47a992970315341224ac0c22e46e7dfc9352cb9a
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-03-14 19:21:36 +0100

    Error when symbol changes - solution 2

--- src/Tablero.jsx
+++ src/Tablero.jsx
@@ -1,45 +1,32 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Celda from './Celda'
 import { whoWins } from './lib/tic_tac_toe'
 
 const CELDAS_INICIALES = [
-  [ '', '', '' ],
-  [ '', '', '' ],
-  [ '', '', '' ]
+  [ null, null, null],
+  [ null, null, null],
+  [ null, null, null]
 ]
 
 function Tablero(props) {
 
   const { jugadores } = props
-  const [ simbolos, setSimbolos ] = useState([jugadores[0].simbolo, jugadores[1].simbolo])
   const [ turno, setTurno ] = useState(0)
   const [ celdas, setCeldas ] = useState(CELDAS_INICIALES)
   const [ ganador, setGanador ] = useState(null)
-
-  useEffect(() => {
-    console.log('Los jugadores han cambiado', jugadores)
-    // Sustituir los simbolos por los nuevos
-    const nuevasCeldas = celdas.map((fila, i) => {
-      return fila.map((celda, j) => {
-        return celda === simbolos[0] ? jugadores[0].simbolo : celda === simbolos[1] ? jugadores[1].simbolo : celda
-      })
-    })
-    setCeldas(nuevasCeldas)
-    // Guardar los simbolos actuales
-    setSimbolos([jugadores[0].simbolo, jugadores[1].simbolo])
-  },[jugadores])
 
   const jugador = jugadores[turno]
 
   function onCeldaClick(i, j) {
     if(ganador) return
-    if (celdas[i][j] !== '') return
+    if (celdas[i][j] !== null) return
 
     const nuevasCeldas = [...celdas]
-    nuevasCeldas[i][j] = jugador.simbolo
+    nuevasCeldas[i][j] = turno
 
     const winner = whoWins(nuevasCeldas)
-    if(winner) {
+    console.log(winner)
+    if(winner !== false && winner !== null) {
       setGanador(jugador)
     }
 
@@ -58,7 +45,7 @@ function Tablero(props) {
               return <Celda
                         key={`${i}-${j}`}
                         onClick={() => onCeldaClick(i,j)}
-                        valor={celda}
+                        valor={jugadores[celda]?.simbolo || ''}
                       />
             })
           })
